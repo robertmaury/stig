ceed6e27c4e1b0ab1318eadfb0a9653a033bfd70
Jerald Sheets <questy@gmail.com>
2014-09-25 12:10:22 -0700

    syntax and style corrections

--- banner/manifests/init.pp
+++ banner/manifests/init.pp
@@ -2,62 +2,65 @@
 # Class: banner
 #
 # Description:
-#	Provides consistent banner across all boxes
+# Provides consistent banner across all boxes
 #
 # Variables:
-#	stig::server
+# stig::server
 #
 # Facts:
-#	None
+# None
 #
 # Files:
-#	banner/files/issue
-#	banner/files/custom.conf
+# banner/files/issue
+# banner/files/custom.conf
 #
 # Templates:
-#	None
+# None
 #
 # Dependencies:
-#	None
+# None
 ############################################################
 class banner {
-	file {
-		"/etc/issue":
-			owner  => root,
-			group  => root,
-			mode   => 444,
-			source => "puppet:///modules/banner/issue",
-	}
+  file {
+    '/etc/issue':
+      ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0444',
+      source => 'puppet:///modules/banner/issue',
+  }
 
-	# We don't run X on our servers
-	if !$stig::server {
-		include banner::gdm
-	}
+  # We don't run X on our servers
+  if !$stig::server {
+    include banner::gdm
+  }
 }
 
 class banner::gdm {
-	file {
-		"/etc/gdm/custom.conf":
-			owner  => root,
-			group  => root,
-			mode   => 444,
-			source => "puppet:///modules/banner/custom.conf";
-		"/var/lib/gdm/.gconf/apps/gdm/simple-greeter/%gconf.xml":
-			owner  => gdm,
-			group  => gdm,
-			mode   => 600,
-			source => "puppet:///modules/banner/%gconf.xml";
-		[
-			"/var/lib/gdm/.gconf/apps/gdm/simple-greeter",
-			"/var/lib/gdm/.gconf/apps/gdm/",
-			"/var/lib/gdm/.gconf/apps/",
-		]:
-			ensure => directory;
+  file {
+    '/etc/gdm/custom.conf':
+      ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0444',
+      source => 'puppet:///modules/banner/custom.conf';
 
-		[
-			"/var/lib/gdm/.gconf/apps/gdm/%gconf.xml",
-			"/var/lib/gdm/.gconf/apps/%gconf.xml",
-		]:
-			ensure => present;
-	}
+    '/var/lib/gdm/.gconf/apps/gdm/simple-greeter/%gconf.xml':
+      ensure => 'present',
+      owner  => 'gdm',
+      group  => 'gdm',
+      mode   => '0600',
+      source => "puppet:///modules/banner/%gconf.xml";
+    [
+      '/var/lib/gdm/.gconf/apps/gdm/simple-greeter',
+      '/var/lib/gdm/.gconf/apps/gdm/',
+      '/var/lib/gdm/.gconf/apps/',
+    ]:
+      ensure => directory;
+    [
+      "/var/lib/gdm/.gconf/apps/gdm/%gconf.xml",
+      "/var/lib/gdm/.gconf/apps/%gconf.xml",
+    ]:
+      ensure => present;
+  }
 }
