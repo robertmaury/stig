cc0e89415a32fc6c43ae145895fc66b4ae31eb5f
Jerald Sheets <questy@gmail.com>
2014-09-25 11:51:29 -0700

    Made syntax and style checking as per latest puppet-lint recommendations and added formatting for clarity.

--- aide/manifests/init.pp
+++ aide/manifests/init.pp
@@ -2,70 +2,72 @@
 # Class: aide
 #
 # Description:
-#	Installs and configures aide, and disables prelinking
+# Installs and configures aide, and disables prelinking
 #
 # Variables:
-#	None
+# None
 #
 # Facts:
-#	None
+# None
 #
 # Files:
-#	aide/files/aide.conf
-#	aide/files/aide.cron
-#	aide/files/aidereset.cron
+# aide/files/aide.conf
+# aide/files/aide.cron
+# aide/files/aidereset.cron
 #
 # Templates:
-#	None
+# None
 #
 # Dependencies:
-#	None
+# None
 ############################################################
 class aide {
 
-	# Install AIDE
-	package {
-		"aide":
-			ensure  => latest,
-			require => Class["yum"];
-	}
+  # Install AIDE
+  package {
+    'aide':
+      ensure  => latest,
+      require => Class['yum'];
+  }
 
-	file {
-		"/etc/aide.conf":
-			owner  => root,
-			group  => wheel,
-			mode   => 600,
-			source => "puppet:///modules/aide/aide.conf";
-		"/etc/cron.daily/aide":
-			owner  => root,
-			group  => root,
-			mode   => 700,
-			source => "puppet:///modules/aide/aide.cron";
-		# This will update the AIDE db every week
-		#"/etc/cron.weekly/aide":
-		#	owner  => root,
-		#	group  => root,
-		#	mode   => 700,
-		#	source => "puppet:///modules/aide/aidereset.cron";
-	}
+  file {
+    '/etc/aide.conf':
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0600',
+      source => 'puppet:///modules/aide/aide.conf';
 
-	augeas {
-		"Disable Prelinking":
-			context => "/files/etc/sysconfig/prelink",
-			changes => [
-				"set PRELINKING no",
-			],
-			notify  => Exec["Turn off Prelinking"];
-	}
+    '/etc/cron.daily/aide':
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0700',
+      source => 'puppet:///modules/aide/aide.cron';
 
-	exec {
-		# This will create the initial AIDE database if it doesn't already exist
-		#"Initialize AIDE DB":
-		#	command => "/usr/sbin/aide -i; /bin/cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz",
-		#	onlyif  => "/usr/bin/test ! -f /var/lib/aide/aide.db.gz";
-		"Turn off Prelinking":
-			command => "/usr/sbin/prelink -ua; /bin/rm /etc/prelink.cache",
-			onlyif  => "/usr/bin/test -e /etc/prelink.cache";
-	}
+    # This will update the AIDE db every week
+    #'/etc/cron.weekly/aide':
+    # owner  => 'root',
+    # group  => 'root',
+    # mode   => '0700',
+    # source => 'puppet:///modules/aide/aidereset.cron';
+  }
 
+  augeas {
+    'Disable Prelinking':
+      context => '/files/etc/sysconfig/prelink',
+      changes => [
+        'set PRELINKING no',
+      ],
+      notify  => Exec['Turn off Prelinking'];
+  }
+
+  exec {
+    # This will create the initial AIDE database if it doesn't already exist
+    #'Initialize AIDE DB':
+    # command => '/usr/sbin/aide -i; /bin/cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz',
+    # onlyif  => '/usr/bin/test ! -f /var/lib/aide/aide.db.gz';
+
+    'Turn off Prelinking':
+      command => '/usr/sbin/prelink -ua; /bin/rm /etc/prelink.cache',
+      onlyif  => '/usr/bin/test -e /etc/prelink.cache';
+  }
 }
