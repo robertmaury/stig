4ee876b8f02ee8951969579f79057eac33303751
Jerald Sheets <questy@gmail.com>
2014-09-26 07:11:01 -0700

    syntax/style corrections

--- iptables/manifests/init.pp
+++ iptables/manifests/init.pp
@@ -4,15 +4,15 @@
 # Description:
 #
 # Variables:
-#	stig::dnsserver
-#	stig::ftpserver
-#	stig::krbserver
-#	stig::ldapserver
-#	stig::mailserver
-#	stig::nfsserver
-#	stig::puppetserver
-#	stig::syslogserver
-#	stig::webserver
+# stig::dnsserver
+# stig::ftpserver
+# stig::krbserver
+# stig::ldapserver
+# stig::mailserver
+# stig::nfsserver
+# stig::puppetserver
+# stig::syslogserver
+# stig::webserver
 #
 # Facts:
 #       None
@@ -27,36 +27,39 @@
 #       None
 ############################################################
 class iptables {
-	file {
-		"/etc/sysconfig/iptables":
-			owner   => root,
-			group   => root,
-			mode    => 600,
-			notify  => Service["iptables"],
-			content => template("iptables/iptables.erb");
-		# To enable ip6tables, uncomment this section and the service section below
-		#"/etc/sysconfig/ip6tables":
-		#	owner   => root,
-		#	group   => root,
-		#	mode    => 600,
-		#	notify  => Service["ip6tables"],
-		#	content => template("iptables/ip6tables.erb");
-		"/etc/sysconfig/iptables-config":
-			owner  => root,
-			group  => root,
-			mode   => 600,
-			notify  => Service["iptables"],
-			source => "puppet:///modules/iptables/iptables-config";
-	}
+  file {
+    '/etc/sysconfig/iptables':
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      notify  => Service['iptables'],
+      content => template('iptables/iptables.erb');
+    # To enable ip6tables, uncomment this section and the service section below
+    #'/etc/sysconfig/ip6tables':
+    # ensure  => 'present',
+    # owner   => 'root',
+    # group   => 'root',
+    # mode    => '0600',
+    # notify  => Service['ip6tables'],
+    # content => template('iptables/ip6tables.erb');
+    '/etc/sysconfig/iptables-config':
+      ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      notify => Service['iptables'],
+      source => 'puppet:///modules/iptables/iptables-config';
+  }
 
-	service {
-		"iptables":
-			ensure => true,
-			enable => true,
-			hasstatus => true;
-		#"ip6tables":
-		#	ensure => true,
-		#	enable => true,
-		#	hasstatus => true;
-	}
+  service {
+    'iptables':
+      ensure    => true,
+      enable    => true,
+      hasstatus => true;
+    #'ip6tables':
+    # ensure    => true,
+    # enable    => true,
+    # hasstatus => true;
+  }
 }
