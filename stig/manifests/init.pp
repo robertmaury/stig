5a6efa863d1956e43af7fb721a5beeca21e6fea8
Jerald Sheets <questy@gmail.com>
2014-09-30 16:34:43 -0400

    syntax & style

--- stig/manifests/init.pp
+++ stig/manifests/init.pp
@@ -1,43 +1,42 @@
-class stig ( $server          = false,
-             $dnsserver       = false,
-             $ftpserver       = false,
-             $webserver       = false,
-             $gitserver       = false,
-             $krbserver       = false,
-             $ldapserver      = false,
-             $mailserver      = false,
-             $needs_gnome     = false,
-             $needs_x11       = false,
-             $nfsbackupserver = false,
-             $nfsserver       = false,
-             $puppetserver    = false,
-             $syslogserver    = false,
-             $yumcanary       = false ) {
+# Main Stig class
+class stig (  $server           = false,
+              $dnsserver        = false,
+              $ftpserver        = false,
+              $webserver        = false,
+              $gitserver        = false,
+              $krbserver        = false,
+              $ldapserver       = false,
+              $mailserver       = false,
+              $needs_gnome      = false,
+              $needs_x11        = false,
+              $nfsbackupserver  = false,
+              $nfsserver        = false,
+              $puppetserver     = false,
+              $syslogserver     = false,
+              $yumcanary        = false ) {
 
-     $dnsservers = "8.8.8.8"
-     $gateway = "10.20.1.1"
-     $syslogserver_ip = "127.0.0.1"
-     $domain = ""
+      $dnsservers = '8.8.8.8'
+      $gateway = '10.20.1.1'
+      $syslogserver_ip = '127.0.0.1'
+      $domain = ''
+      $sudoers = '%admins  ALL=(ALL)       ALL'
 
-     $sudoers = "%admins  ALL=(ALL)       ALL"
-
-#     class { "fstab": }
-     class { "init": }
-     class { "iptables": }
-     class { "kernel": }
-#     class { "named": }
-#     class { "networking": }
-#     class { "nfs": nfs_server_fqdn => $nfs_server_fqdn; }
-     class { "ntp": }
-     class { "packages": }
-     class { "pam": }
-     class { "postfix": }
-#     class { "puppet": }
-     class { "rsyslog": }
-     class { "services": }
-     class { "ssh": }
-     class { "sudo": }
-     class { "sysctl": }
-     class { "yum": }
-
+#     class { 'fstab': }
+      class { 'init': }
+      class { 'iptables': }
+      class { 'kernel': }
+#     class { 'named': }
+#     class { 'networking': }
+#     class { 'nfs': nfs_server_fqdn => $nfs_server_fqdn; }
+      class { 'ntp': }
+      class { 'packages': }
+      class { 'pam': }
+      class { 'postfix': }
+#     class { 'puppet': }
+      class { 'rsyslog': }
+      class { 'services': }
+      class { 'ssh': }
+      class { 'sudo': }
+      class { 'sysctl': }
+      class { 'yum': }
 }
